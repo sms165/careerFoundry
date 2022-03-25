46ceb0d89a4592e683607feab3da5828dace0fc3
sms165 <61697688+sms165@users.noreply.github.com>
2022-03-25 12:06:34 +0100

    validate form email and phone number with JavaScript

--- js/main.js
+++ js/main.js
@@ -1,39 +1,87 @@
-function validateEmail() {
-    let value = emailInput.value;
 
+//validate contact form
+(function() {
+    let form = document.querySelector('#contact-form');
+    let emailInput = document.querySelector('#email');
+    let phoneInput = document.querySelector('#phone');
+   
+    function showErrorMessage(input, message) {
+        let container = input.parentElement;
+    
+        //Remove an existing error
+        let error = container.querySelector('.error-message');
+        if (error) {
+            container.removeChild(error);
+            
+        }
+    
+        //Add the error if the message isn't empty
+        if (message) {
+            let error= document.createElement('div');
+            error.classList.add('error-message');
+            error.innerText=message;
+            container.appendChild(error);
+            
+        }
+        
+    }
+
+    function validateEmail() {
+        let value = emailInput.value;
+
+        if (!value) {
+            showErrorMessage(emailInput, 'Email is a required field.');
+            return false;
+        }
+    
+        if (value.indexOf('@')=== -1) {
+            showErrorMessage(emailInput, 'You must enter a valid email address.');
+            return false 
+        }
+    
+        showErrorMessage(emailInput, null);
+        return true;
+    }
+    
+    function validatePhone() {
+        let value = phoneInput.value;
+        let phoneNum =  /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+        
     if (!value) {
-        showErrorMessage(emailInput, 'Email is a required field.');
+        showErrorMessage(phoneInput, 'Phone number is a required field');
+        return false;      
+    }
+
+    if(!value.match(phoneNum)){
+        showErrorMessage(phoneInput, 'Enter a valid phone number (XXX-XXX-XXXX');
         return false;
+        
     }
 
-    if (value.indexOf('@')=== -1) {
-        showErrorMessage(emailInput, 'You must enter a valid email address.');
-        return false 
-    }
-
-    showErrorMessage(emailInput, null);
+    showErrorMessage(phoneInput, null);
     return true;
 }
 
-function showErrorMessage(input, message) {
-    let container = input.parentElement;
 
-    //Remove an existing error
-    let error = container.querySelector('.error-message');
-    if (error) {
-        container.removeChild(error);
-        
-    }
-
-    //Add the error if the message isn't empty
-    if (message) {
-        let error= document.createElement('div');
-        error.classList.add('error-message');
-        error.innerText=message;
-        container.appendChild(error);
-        
+    
+    function validateForm() {
+        let isValidEmail = validateEmail();
+        let isValidPhone = validatePhone
+        return isValidEmail && isValidPhone;
     }
     
-}
+    form.addEventListener('submit', (e) => {
+      e.preventDefault(); // Do not submit to the server
+      if (validateForm()) {
+        alert('Success!');
+      }
+    });
 
-emailInput.addEventListener('input', validateEmail);
+    emailInput.addEventListener('input', validateEmail);
+    phoneInput.addEventListener('input', validatePhone);
+  })();
+
+
+
+
+
